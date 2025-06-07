3495e0214a84fabab732ed7f04889dcf3cc75272
Aleksei Dmitriev <aleshadmi@mail.ru>
2025-06-07 19:31:28 +0300

    Play with possible data storage

--- data/generate_data.py
+++ data/generate_data.py
@@ -32,3 +32,18 @@ try:
     print(user)
 except ValidationError as e:
     print(e)
+
+
+
+
+# Пример работы с datasets
+from datasets import load_dataset
+
+dataset = load_dataset( "json", data_files="tests_datasets/my_data.jsonl", split="train")
+
+print("Size of dataset:", len(dataset))
+
+first_record = dataset[0]
+print("First record:", first_record)
+print("Full dataset \n", dataset)
+
